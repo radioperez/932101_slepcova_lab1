9c0c248c1ad5ac2dabcbd8a240b870c6d07f1feb
Raz <slepcovas144@gmail.com>
2024-12-20 12:09:16 +0700

    Added multithread

--- winget.py
+++ winget.py
@@ -1,64 +1,121 @@
 import sys
-from urllib.request import urlretrieve, urlopen
+from urllib.request import urlopen, Request
+import asyncio
+import concurrent.futures
+import functools
+import urllib.request
+import os
+import threading
 
 def parser(args):
     args = args[1:]
-    if len(args) < 1 or len(args) > 3:
+    if len(args) > 3:
         print("Incorrect usage. Try --help")
         sys.exit()
     
-    option, url, filename = True, None, None
-    if args[0] in ('-h', '--help'):
-        print("""
+    silent, url, filename = False, None, None
+    if args:
+        if args[0] in ('-h', '--help'):
+            print("""
               Usage: winget.py [OPTION] URL [FILENAME]
               Скачать файл лежащий в URL под названием FILENAME.
               Если FILENAME не указан, файл сохранится с названием в URL.
-              
+              Если URL не указан, будет запущен stdin, файл сохранится с названием в URL.
+
               OPTIONS:
                 -h --help Показывает это сообщение
                 -s --silent  Прячет прогрессбар""")
-        sys.exit()
-    elif args[0] in ('-s', '--silent'):
-        option = False
+            sys.exit()
+        elif args[0] in ('-s', '--silent'):
+            silent = True
+            args = args[1:]
+    
+    if args:
+        url = args[0]
         args = args[1:]
-    url = args[0]
-    args = args[1:]
+    else:
+        url = input("URL: ")
+    
     if args:
         filename = args[0]
     else:
-        filename = url.split('/')[-1]
-    return option, url, filename
+        filename = filename = url.split('/')[-1]
 
-def filegetter(option, url, filename):
-    try:
-        with urlopen(url) as response:
-            length = float(response.headers['content-length'])
-            downloaded = 0.0
-            data_blocks = []
-            
-            while True:
-                block = response.read(1024)
-                data_blocks.append(block)
-                downloaded += len(block)
-                percent = int(downloaded * 100 / length)
-                if option:
-                    print(end='\r', flush=True)
-                    print(f'[{f'{percent}%':>^{percent}}{('-'*(100-int(percent)))}]', end='\r', flush=True)
+    return silent, url, filename
 
-                if not len(block):
-                    if option: print('\n')
-                    break
-            data = b''.join(data_blocks)
-            response.close()
-            if option: print('Загрузка выполнена!')
-        with open(filename, 'wb') as f:
-            f.write(data)
+downloaded_bytes = 0
+downloaded_lock = threading.Lock()
 
-    except ValueError:
-        print("Неправильно введен URL. --help")
-    except Exception as e:
-        print("Ошибка сети.", e.__cause__)
+async def get_size(url):
+    with urlopen(url) as response:
+        size = int(response.headers['content-length'])
+    return size
+
+def download_range(silent, url, start, end, output, file_size):
+    global downloaded_bytes
+    headers = {'Range': f'bytes={start}-{end}'}
+    req = Request(url, headers=headers)
     
+    with urlopen(req) as response:
+        with open(output, 'wb') as f:
+            while True:
+                chunk = response.read(1024)
+                if not chunk:
+                    break
+                f.write(chunk)
+                with downloaded_lock:
+                    downloaded_bytes += len(chunk)
+                    if not(silent): print_progress(downloaded_bytes, file_size)
+
+
+def print_progress(downloaded, file_size):
+    bar = 100
+    percent = int(downloaded * bar / file_size)
+    print(end='\r', flush=True)
+    print(f'[{f'{percent}%':>^{percent}}{('ⴾ'*(bar-percent))}]', end='\r', flush=True)
+
+async def download(silent, run, loop, url, output, chunk_size=1024*1024):
+    file_size = await get_size(url)
+    chunks = range(0, file_size, chunk_size)
+
+    tasks = [
+        run(
+            download_range,
+            silent,
+            url,
+            start,
+            start + chunk_size - 1,
+            f'{output}.part{i}',
+            file_size
+        )
+        for i, start in enumerate(chunks)
+    ]
+
+    await asyncio.wait(tasks)
+
+    with open(output, 'wb') as o:
+        for i in range(len(chunks)):
+            chunk_path = f'{output}.part{i}'
+
+            with open(chunk_path, 'rb') as s:
+                o.write(s.read())
+
+            os.remove(chunk_path)
+    print(flush=True)
+    
+
 if __name__ == '__main__':
-    option, url, filename = parser(sys.argv)
-    filegetter(option, url, filename)
+    silent, url, filename = parser(sys.argv)
+
+    executor = concurrent.futures.ThreadPoolExecutor()
+    loop = asyncio.new_event_loop()
+    run = functools.partial(loop.run_in_executor, executor)
+
+    asyncio.set_event_loop(loop)
+
+    try:
+        loop.run_until_complete(
+            download(silent, run, loop, url, filename)
+        )
+    finally:
+        loop.close()
